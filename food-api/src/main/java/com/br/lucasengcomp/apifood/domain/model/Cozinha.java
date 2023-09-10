9e3ae98bf739cb69155a1a1e0927145589ef3c74
Lucas Galvao <lucas.engcomp@outlook.com>
2023-09-10 13:19:03 -0300

    Adicionado estratégia de geração de id

--- food-api/src/main/java/com/br/lucasengcomp/apifood/domain/model/Cozinha.java
+++ food-api/src/main/java/com/br/lucasengcomp/apifood/domain/model/Cozinha.java
@@ -4,9 +4,7 @@ package com.br.lucasengcomp.apifood.domain.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
@@ -16,6 +14,7 @@ import java.util.Objects;
 public class Cozinha {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String nome;
--- food-api/src/main/java/com/br/lucasengcomp/apifood/domain/model/Restaurante.java
+++ food-api/src/main/java/com/br/lucasengcomp/apifood/domain/model/Restaurante.java
@@ -3,10 +3,7 @@ package com.br.lucasengcomp.apifood.domain.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Objects;
 
@@ -17,6 +14,7 @@ import java.util.Objects;
 public class Restaurante {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
 
