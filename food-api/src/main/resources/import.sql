fcc0faa245d4fe0986f37aa7497bc0477fac2347
Lucas Galvao <lucas.engcomp@outlook.com>
2023-09-10 14:49:55 -0300

    Seed nas tabelas tbl_restaurante e tbl_cozinha

--- food-api/src/main/resources/import.sql
+++ food-api/src/main/resources/import.sql
@@ -1,3 +1,6 @@
 INSERT INTO tbl_cozinha (nome) VALUES ('Brasileira');
 INSERT INTO tbl_cozinha (nome) VALUES ('Mexicana');
 INSERT INTO tbl_cozinha (nome) VALUES ('Americana');
+
+INSERT INTO tbl_restaurante (nome, taxa_frete) VALUES ('Président', 10.58);
+INSERT INTO tbl_restaurante (nome, taxa_frete) VALUES ('Cão velho', 8.73);
