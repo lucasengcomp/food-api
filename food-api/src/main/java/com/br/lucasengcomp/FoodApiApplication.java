4bd12c4e49f10a21be88258b4fd2c519f4bc5bde
Lucas Galvao <lucas.engcomp@outlook.com>
2023-09-10 07:38:39 -0300

    :tada: Projeto criado

---
+++ food-api/src/main/java/com/br/lucasengcomp/FoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.lucasengcomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodApiApplication.class, args);
+	}
+
+}
