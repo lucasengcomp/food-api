6cc0df1c1bb0a0732137c7250ee739498bc6fe87
Lucas Galvao <lucas.engcomp@outlook.com>
2023-09-10 15:19:37 -0300

    Adicionando validações para campos nulos

--- food-api/src/main/resources/import.sql
+++ food-api/src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO tbl_cozinha (nome) VALUES ('Brasileira');
-INSERT INTO tbl_cozinha (nome) VALUES ('Mexicana');
-INSERT INTO tbl_cozinha (nome) VALUES ('Americana');
+INSERT INTO tbl_cozinha (id, nome) VALUES (1, 'Brasileira');
+INSERT INTO tbl_cozinha (id, nome) VALUES (2, 'Mexicana');
+INSERT INTO tbl_cozinha (id, nome) VALUES (3, 'Italiano');
 
-INSERT INTO tbl_restaurante (nome, taxa_frete) VALUES ('Président', 10.58);
-INSERT INTO tbl_restaurante (nome, taxa_frete) VALUES ('Cão velho', 8.73);
+INSERT INTO tbl_restaurante (nome, taxa_frete) VALUES ('Président', 10.58, 3);
+INSERT INTO tbl_restaurante (nome, taxa_frete) VALUES ('Cão velho', 8.73, 1);
