22ba99c098d81c880ce5fe89414370f9ecfbf6bf
Lucas Galvao <lucas.engcomp@outlook.com>
2023-09-11 11:14:47 -0300

    Adicionado get de todas as cozinhas

---
+++ food-api/src/main/java/com/br/lucasengcomp/apifood/domain/controller/CozinhaController.java
@@ -0,0 +1,23 @@
+package com.br.lucasengcomp.apifood.domain.controller;
+
+import com.br.lucasengcomp.apifood.domain.model.Cozinha;
+import com.br.lucasengcomp.apifood.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository repository;
+
+    @GetMapping
+    public List<Cozinha> listar() {
+        return repository.findAll();
+    }
+}
