78ed2ee9e378e6deb50f7cd1f1eac22e147cce12
Lucas Galvao <lucas.engcomp@outlook.com>
2023-09-11 15:34:12 -0300

    Adicionado busca por id de Cozinha

--- food-api/src/main/java/com/br/lucasengcomp/apifood/domain/controller/CozinhaController.java
+++ food-api/src/main/java/com/br/lucasengcomp/apifood/domain/controller/CozinhaController.java
@@ -5,6 +5,7 @@ import com.br.lucasengcomp.apifood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class CozinhaController {
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Cozinha> listar() {
         return repository.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Cozinha cozinha(@PathVariable Long id) {
+        return repository.findById(id).get();
     }
 }
