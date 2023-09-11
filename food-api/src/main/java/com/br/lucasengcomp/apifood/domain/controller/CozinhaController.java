68b933e383191a93d2c6c97ef945b6fbedfc59da
Lucas Galvao <lucas.engcomp@outlook.com>
2023-09-11 15:27:24 -0300

    Adicionado lib para listagem no padrão xml

--- food-api/src/main/java/com/br/lucasengcomp/apifood/domain/controller/CozinhaController.java
+++ food-api/src/main/java/com/br/lucasengcomp/apifood/domain/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.br.lucasengcomp.apifood.domain.controller;
 import com.br.lucasengcomp.apifood.domain.model.Cozinha;
 import com.br.lucasengcomp.apifood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ public class CozinhaController {
     @Autowired
     private CozinhaRepository repository;
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Cozinha> listar() {
         return repository.findAll();
     }
